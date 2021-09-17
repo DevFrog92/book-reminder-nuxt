fccca48cb6f859d8ce3ec26ae3b978dc718cb91d
DevFrog92 <yongsu1481@gmail.com>
2021-09-17 23:31:39 +0900

    add : eslint for code formatting refactor : apply eslint

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,19 +1,19 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'book-reminder-nuxt',
+    title: "book-reminder-nuxt",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8", },
+      { name: "viewport", content: "width=device-width, initial-scale=1", },
+      { hid: "description", name: "description", content: "", },
+      { name: "format-detection", content: "telephone=no", }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico", }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -30,7 +30,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/eslint
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module"
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
@@ -39,5 +39,5 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
 }
