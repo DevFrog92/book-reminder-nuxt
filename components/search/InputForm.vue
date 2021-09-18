d8352d517886daa64433ee7f4f4d08af82c0024c
DevFrog92 <yongsu1481@gmail.com>
2021-09-18 15:34:17 +0900

    add : search input form component

---
+++ components/search/InputForm.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search-input-form">
+    <input
+      v-model="bookTitle"
+      type="text"
+      placeholder="도서명 또는 작가의 이름을 입력해주세요"
+    >
+    <button @click="searchBookInfo">
+      Search
+    </button>
+  </div>
+</template>
+
+<script>
+import { fetchBookInfo, } from "~/api/index"
+export default {
+  data() {
+    return {
+      bookTitle: "",
+    }
+  },
+  methods: {
+    async searchBookInfo() {
+      if (! this.bookTitle.trim().length) {
+        alert("뭐라도 적어라 이놈아!!")
+
+        return
+      }
+
+      try {
+        const response = await fetchBookInfo(this.bookTitle)
+
+        console.log(response)
+      } catch (error) {
+        throw new Error(error)
+      }
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ components/search/books.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-items-wrapper">
+    this is items view
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
