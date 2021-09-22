05687d827d8f208142b98968b0727b6ad8aa5736
DevFrog92 <yongsu1481@gmail.com>
2021-09-22 20:37:06 +0900

    add : client only tag for text editor

--- components/report/MarkDown.vue
+++ components/report/MarkDown.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="mark-down-wrapper">
-    <editor
-      align="left"
-      height="600px"
-    />
+    <ClientOnly>
+      <editor
+        v-model="content"
+        :initial-value="initalValue"
+        :options="options"
+        align="left"
+        height="600px"
+      />
+      <button @click="write">
+        작성
+      </button>
+    </ClientOnly>
   </div>
 </template>
 
@@ -11,7 +19,21 @@
 export default {
   data() {
     return {
+      initalValue: "이곳에 작성해주세요!",
+      options: {
+        language: "ko",
+        hooks: { addImageBlobHook: this.addImageBlobHook, },
+      },
+      content: "",
     }
+  },
+  methods: {
+    addImageBlobHook() {
+      // TODO: 추후 백엔드 설계 후 url로만 대체할 수 있도록 구현 필요
+    },
+    write() {
+      console.log(this.content)
+    },
   },
 
 }
