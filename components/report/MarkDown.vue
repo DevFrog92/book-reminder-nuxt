b2791c6255336285a095c054a5f4391ac75630ae
DevFrog92 <yongsu1481@gmail.com>
2021-09-22 20:17:23 +0900

    add : toast ui editor

---
+++ components/report/MarkDown.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="mark-down-wrapper">
+    <editor
+      align="left"
+      height="600px"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+
+}
+</script>
+
+<style>
+
+</style>
