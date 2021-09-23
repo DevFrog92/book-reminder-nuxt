ceb7f0e6f2c7af7abd86a2fe8d6d5d1cc5721489
DevFrog92 <yongsu1481@gmail.com>
2021-09-23 23:24:56 +0900

    add : logo svg

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,23 +1,96 @@
 <template>
   <nav class="nav-wrapper">
-    <Nuxt-link to="/report">
-      report
+    <div class="logo-wrapper">
+      <div
+        class="link logo-link"
+        to="/"
+        @click="moveToHome"
+      />
+    </div>
+    <Nuxt-link
+      class="link"
+      to="/report"
+    >
+      독후감
     </Nuxt-link>
-    <Nuxt-link to="/">
-      Home
-    </Nuxt-link>
-    <Nuxt-link to="/search">
-      search
+
+    <Nuxt-link
+      class="link"
+      to="/search"
+    >
+      도서검색
     </Nuxt-link>
   </nav>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    moveToHome() {
+      this.$router.push("/")
+    },
+  },
 }
 </script>
 
 <style scoped>
+@font-face {
+    font-family: 'BMDOHYEON';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/BMDOHYEON.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
+.nav-wrapper {
+  height: 6rem;
+  display: flex;
+  font-family: 'BMDOHYEON';
+  align-items: center;
+  justify-content: flex-end;
+  border-bottom: 1px solid rgba(0,0,0,0.7);
+  position: relative;
+}
+
+.logo-wrapper {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%,0);
+  height: 100%;
+  width: 6rem;
+  height: 6rem;
+}
+
+.link {
+  margin-right: 1.5rem;
+  color: rgba(0,0,0,0.8);
+  text-decoration: none;
+}
+
+.logo-link {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  display: inline-block;
+  background-image: url('~assets/images/logo.svg');
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-size: contain;
+}
+
+.logo-link::after {
+  content: "";
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%,-50%);
+  width: 6rem;
+  height: 3rem;
+  cursor: pointer;
+}
+
+.link.nuxt-link-exact-active {
+  color: #2AC8A2;
+  text-decoration: none;
+}
 
 </style>
