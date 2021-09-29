0381d067b67661c6772656e70e9d6d955fe7b73f
DevFrog92 <yongsu1481@gmail.com>
2021-09-30 01:01:28 +0900

    design : update book item

--- components/search/BookItem.vue
+++ components/search/BookItem.vue
@@ -1,23 +1,11 @@
 <template>
   <div class="book-item-wrapper">
-    <div class="book-thumbnail">
-      <img
-        :src="bookItem.thumbnail"
-        alt="book cover thumbnail"
-        class="book-thumbnail-img"
-      >
-    </div>
-    <div class="book-info-desc">
-      <h5 class="book-info-title">
-        {{ bookItem.title }}
-      </h5>
-      <div class="book-info-author">
-        {{ bookItem.authors[0] }}
-      </div>
-      <button @click="moveToReport">
-        독후감 작성하기
-      </button>
-    </div>
+    <img
+      :src="bookItem.thumbnail"
+      alt="book cover thumbnail"
+      class="book-thumbnail-img"
+      @click="moveToReport"
+    >
   </div>
 </template>
 
@@ -41,7 +29,7 @@ export default {
 
 <style scoped>
 .book-item-wrapper{
-  min-height: 10rem;
+  min-height: 11rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -50,43 +38,10 @@ export default {
   cursor: pointer;
 }
 
-.book-thumbnail {
-  position: absolute;
-}
-
 .book-thumbnail-img {
   display: inline-block;
-  width: 100%;
-  height: 100%;
   object-fit:contain;
   -webkit-box-shadow: 5px 5px 15px 0px #BABABA;
   box-shadow: 5px 5px 15px 0px #BABABA;
 }
-
-.book-info-desc {
-  width:100%;
-  height: 100%;
-  position: absolute;
-  color: #fff;
-  background-color: rgba(0,0,0,0.8);
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  padding: 0 1rem;
-  align-items: center;
-  opacity: 0;
-  transition: all .4s ease-out;
-  z-index: -2;
-}
-
-.book-thumbnail-img:hover .book-info-desc{
-  opacity: 1;
-  z-index: 3;
-}
-
-.book-info-title {
-  font-weight: 600;
-  font-size: 1.1rem;
-}
-
 </style>
